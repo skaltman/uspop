fc341f8174c321430e750b7c9fdfc7bf50ab4582
skaltman <skaltman@stanford.edu>
2020-09-04 14:10:56 -0700

    add code coverage

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- badges: start -->
+[![Codecov test coverage](https://codecov.io/gh/skaltman/uspop/branch/master/graph/badge.svg)](https://codecov.io/gh/skaltman/uspop?branch=master)
+<!-- badges: end -->
--- tests/testthat/test-population.R
+++ tests/testthat/test-population.R
@@ -1,8 +1,9 @@
-# x <- population("us")
-#
-# test_that("population() returns a data frame", {
-#   expect_s3_class(x, "data.frame")
-# })
+
+test_that("population() returns a data frame", {
+  x <- population("us")
+
+  expect_s3_class(x, "data.frame")
+})
 
 test_that("population() returns the right columns", {
   x <- population("us")
@@ -18,11 +19,14 @@ test_that("population() returns the right types", {
   expect_type(x$pop, "double")
 })
 
-test_that("population() works for US, states, and counties", {
-  x <- population("us")
-  expect_true(unique(x$region) == "United States")
-  # make population("state:*") work
-  # also get rid of message from as_tibble
+test_that("population() returns the correct FIPS", {
+  us <- population("us")
+  county <- population("county")
+  state <- population("state")
+
+  expect_true(all(us$fips == "0L"))
+  expect_true(all(stringr::str_length(state$fips) == 2))
+  expect_true(all(stringr::str_length(county$fips) == 5))
 })
 
 test_that("population() throws an error for unallowed `geography` values", {
